f87edc982351e6639d2160afde089e6cc5579262
Dennis Weiss <weiss.dennis98@yahoo.de>
2018-06-30 13:15:08 +0200

    Add matrix to show

--- scripts/index.js
+++ scripts/index.js
@@ -7,23 +7,27 @@ const axisColor = '#000000'
 const iHat = 'red'
 const jHat = 'blue'
 
-let windowWidth = window.innerWidth
-let windowHeight = window.innerHeight
+let parentDivWidth = document.getElementById('coordinate-system-div').offsetWidth
+let parentDivHeight = document.getElementById('coordinate-system-div').offsetHeight
 
 const coordinateSystem = document.getElementById('coordinate-system')
 const context = coordinateSystem.getContext('2d')
 
-let matrix = [[-3, 4], [3, -1]]
+const matrixCanvas = document.getElementById('matrix')
+const matrixContext = matrixCanvas.getContext('2d')
+
+let matrix = [[-3.56, 4.1], [3.78, -1]]
 
 const updateDimensions = () => {
-    windowWidth = window.innerWidth
-    windowHeight = window.innerHeight
+    parentDivWidth = document.getElementById('coordinate-system-div').offsetWidth
+    parentDivHeight = document.getElementById('coordinate-system-div').offsetHeight
 
-    coordinateSystem.width = window.innerWidth - 2 * marginHorizontal
-    coordinateSystem.height = window.innerHeight - 2 * marginVertical
+    coordinateSystem.width = parentDivWidth - 2 * marginHorizontal
+    coordinateSystem.height = parentDivHeight - 2 * marginVertical
 
     drawCoordinates()
     drawBaseVectors()
+    setInfo()
 }
 
 const drawCoordinates = () => {
@@ -58,12 +62,7 @@ const drawBaseVectors = () => {
     context.strokeStyle = iHat
     context.lineWidth = 5
     context.moveTo(origin[0], origin[1])
-    context.lineTo(origin[0] + matrix[0][0] * scale, origin[1] + matrix[0][1] * scale)
-    // context.lineTo(arrowHeadLength * (-2 * matrix[0][0] / Math.sqrt(5) + -matrix[0][1] / Math.sqrt(5)) + origin[0] + matrix[0][0] * scale,
-    //     arrowHeadLength * (matrix[0][0] / Math.sqrt(5) + -2 * matrix[0][1] /Math.sqrt(5)) + origin[1] + matrix[0][1] * scale)
-    context.lineTo(origin[0] + matrix[0][0] * scale, origin[1] + matrix[0][1] * scale)
-    // context.lineTo(arrowHeadLength * (-2 * matrix[0][0] / Math.sqrt(5) + matrix[0][1] / Math.sqrt(5)) + origin[0] + matrix[0][0] * scale,
-    //     arrowHeadLength * (-matrix[0][0] / Math.sqrt(5) + -2 * matrix[0][1] /Math.sqrt(5)) + origin[1] + matrix[0][1] * scale)
+    context.lineTo(origin[0] + matrix[0][0] * scale, origin[1] - matrix[0][1] * scale)
     context.stroke()
 
     //draw j hat
@@ -75,45 +74,98 @@ const drawBaseVectors = () => {
     context.stroke()
 }
 
+const resetCanvas = () => {
+    context.clearRect(0, 0, coordinateSystem.width, coordinateSystem.height)
+    matrixContext.clearRect(0, 0, matrixCanvas.width, matrixCanvas.height)
+}
+
 const setIHat = event => {
-    console.log('event')
     const origin = [Math.floor(coordinateSystem.width / 2), Math.floor(coordinateSystem.height / 2)]
 
     const x = (event.offsetX - origin[0]) / scale
-    const y = (event.offsetY - origin[1]) / scale
+    const y = -(event.offsetY - origin[1]) / scale
 
     matrix = [[x, y], matrix[1]]
+
+    resetCanvas()
+    drawCoordinates()
     drawBaseVectors()
+    setInfo()
+}
+
+const setJHat = event => {
+    const origin = [Math.floor(coordinateSystem.width / 2), Math.floor(coordinateSystem.height / 2)]
+
+    const x = (event.offsetX - origin[0]) / scale
+    const y = -(event.offsetY - origin[1]) / scale
+
+    matrix = [matrix[0], [x, y]]
+
+    resetCanvas()
+    drawCoordinates()
+    drawBaseVectors()
+    setInfo()
 }
 
 const handleCoordinateSystemClick = event => {
     const origin = [Math.floor(coordinateSystem.width / 2), Math.floor(coordinateSystem.height / 2)]
     const x = event.offsetX
     const y = event.offsetY
-    const iHat = [origin[0] + matrix[0][0] * scale, origin[1] + matrix[0][1] * scale]
-    const jHat = [origin[0] + matrix[0][0] * scale, origin[1] + matrix[0][1] * scale]
+    const iHat = [origin[0] + matrix[0][0] * scale, origin[1] - matrix[0][1] * scale]
+    const jHat = [origin[0] + matrix[1][0] * scale, origin[1] - matrix[1][1] * scale]
 
-    console.log(x, y, iHat)
+    console.log(x, y, iHat, jHat)
 
     if ((x - iHat[0]) * (x - iHat[0]) + (y - iHat[1]) * (y - iHat[1]) < scale * scale * 0.3 * 0.3) {
-        console.log('yes')
         coordinateSystem.addEventListener('mousemove', setIHat)
+    } else if ((x - jHat[0]) * (x - jHat[0]) + (y - jHat[1]) * (y - jHat[1]) < scale * scale * 0.3 * 0.3) {
+        coordinateSystem.addEventListener('mousemove', setJHat)
     }
 }
 
 const handleCoordinateSystemMousUp = event => {
     coordinateSystem.removeEventListener('mousemove', setIHat)
+    coordinateSystem.removeEventListener('mousemove', setJHat)
+}
+
+const setInfo = () => {
+    matrixContext.font = '30px Time New Roman'
+    matrixContext.fillText('A =', 1, 70)
+
+    matrixContext.beginPath()
+    matrixContext.moveTo(65, 5)
+    matrixContext.lineTo(60, 5)
+    matrixContext.lineTo(60, 125)
+    matrixContext.lineTo(65, 125)
+    matrixContext.strokeStyle = '#000000'
+    matrixContext.stroke()
+
+    matrixContext.beginPath()
+    matrixContext.moveTo(215, 5)
+    matrixContext.lineTo(220, 5)
+    matrixContext.lineTo(220, 125)
+    matrixContext.lineTo(215, 125)
+    matrixContext.strokeStyle = '#000000'
+    matrixContext.stroke()
+
+    matrixContext.font = '24px Time New Roman'
+    matrixContext.fillText(matrix[0][0], 75, 35)
+    matrixContext.fillText(matrix[0][1], 75, 105)
+    matrixContext.fillText(matrix[1][0], 155, 35)
+    matrixContext.fillText(matrix[1][1], 155, 105)
 }
 
 
-coordinateSystem.width = window.innerWidth - 2 * marginHorizontal
-coordinateSystem.height = window.innerHeight - 2 * marginVertical
+
+coordinateSystem.width = parentDivWidth - 2 * marginHorizontal
+coordinateSystem.height = parentDivHeight - 2 * marginVertical
 
 window.addEventListener('resize', updateDimensions)
 coordinateSystem.addEventListener('mousedown', handleCoordinateSystemClick)
-coordinateSystem.addEventListener('mouseup', handleCoordinateSystemMousUp)
+window.addEventListener('mouseup', handleCoordinateSystemMousUp)
 
 
 drawCoordinates()
 drawBaseVectors()
+setInfo()
 
